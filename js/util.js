f00efb6871970df0c2e24a624114787e65869913
dklobov <d.k.lobov@gmail.com>
2026-03-19 14:03:44 +0300

    удаление пробелов, не проходил тест

--- js/util.js
+++ js/util.js
@@ -1,27 +1,26 @@
 // случайное целое число
 export const getRandomInteger = (min, max) => {
-    const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-    const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-    const result = Math.random() * (upper - lower + 1) + lower;
-  
-    return Math.floor(result);
-  };
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
 
 // случайное неповторяющееся число из диапазона
 export const getGeneratedFromToUniq = (min, max) => {
-    const previousValues = [];
-    return function () {
-      if (previousValues.length >= max - min + 1) {
-        return;
-      }
-      let currentValue = getRandomInteger (min, max);
-      while (previousValues.includes(currentValue)) {
-        currentValue = getRandomInteger(min, max);
-      }
-      previousValues.push(currentValue);
-      return currentValue;
-    };
+  const previousValues = [];
+  return function () {
+    if (previousValues.length >= max - min + 1) {
+      return;
+    }
+    let currentValue = getRandomInteger (min, max);
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
   };
+};
 
 // обёртка для случайного числа из диапазона (с повторениями)
 export const getGeneratedFromTo = (min, max) => () => getRandomInteger(min, max);
