da7365ab6fb4d288ad934d2ba2cdfb76c8f00613
dklobov <d.k.lobov@gmail.com>
2026-04-06 18:44:16 +0300

    добавление form.js в точку входа

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ import { getPhotos } from './setup.js';
 import { renderPhotos } from './render-photos.js';
 import { PHOTOS_NUMBER } from './data.js';
 import { initModal } from './big-picture/init-modal.js';
+import './upload-photo.js';
+import './form.js';
 
 const photos = getPhotos(PHOTOS_NUMBER);
 renderPhotos(photos);
-initModal (photos);
+initModal(photos);
