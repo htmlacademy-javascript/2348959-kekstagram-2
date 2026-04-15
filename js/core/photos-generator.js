d2cf793099f84b528e368a4b58f2b4be4875c148
dklobov <d.k.lobov@gmail.com>
2026-04-15 17:30:06 +0300

    удаление комментов

--- js/core/photos-generator.js
+++ js/core/photos-generator.js
@@ -1,5 +1,3 @@
-// генерация массива фото с комментариями
-
 import {
   getRandomInteger,
   getGeneratedFromToUniq,
@@ -7,7 +5,6 @@ import {
   getElementArray
 } from './util.js';
 
-// импорт диапазонов и массивов данных
 import {
   MIN_COMMENTS, MAX_COMMENTS,
   MIN_LIKES, MAX_LIKES,
@@ -17,19 +14,16 @@ import {
   DESCRIPTIONS, NAMES, COMMENTS
 } from './data.js';
 
-// создание генераторов (единожды при загрузке модуля)
 const photosId = getGeneratedFromToUniq(MIN_PHOTO_ID, MAX_PHOTO_ID);
 const photoURL = getGeneratedFromToUniq(MIN_PHOTO_ID, MAX_PHOTO_ID);
 const commentId = getGeneratedFromToUniq(1, 9999);
 const avatarId = getGeneratedFromTo(MIN_AVATAR_ID, MAX_AVATAR_ID);
 const getURL = () => `photos/${photoURL()}.jpg`;
 
-// генераторы случайных элементов из массивов
 const randomDescription = getElementArray(DESCRIPTIONS);
 const randomName = getElementArray(NAMES);
 const randomComment = getElementArray(COMMENTS);
 
-// создание ОДНОГО объекта-коммента
 const createComment = () => ({
   id: commentId(),
   avatar: `img/avatar-${avatarId()}.svg`,
@@ -37,13 +31,11 @@ const createComment = () => ({
   name: randomName()
 });
 
-// создание массива комментов
 const createCommentsArray = () => {
   const count = getRandomInteger(MIN_COMMENTS, MAX_COMMENTS);
   return Array.from ({length: count}, () => createComment());
 };
 
-// ГЛАВНАЯ ФУНКЦИЯ: создание массива фотографий с массивом комментариев
 export const getPhotos = () => Array.from(
   { length: PHOTOS_NUMBER },
   () => ({
