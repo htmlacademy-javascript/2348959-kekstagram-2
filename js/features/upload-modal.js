4a878046bde672883f64e0215aaab42e2b741692
dklobov <d.k.lobov@gmail.com>
2026-04-18 11:15:52 +0300

    рефакторинг

--- js/features/upload-modal.js
+++ js/features/upload-modal.js
@@ -1,11 +1,11 @@
-import { createModalController } from '../widgets/modal-controller.js';
 import { resetScale } from './upload-scale.js';
 import { resetEffects } from './upload-effects.js';
+import { createModalController } from '../widgets/modal-controller.js';
 
 const imgUpload = document.querySelector('.img-upload__overlay');
 const fileInput = document.querySelector('#upload-file');
-const closeButton = document.querySelector('.img-upload__cancel');
 const imgPreview = document.querySelector('.img-upload__preview img');
+const closeButton = document.querySelector('.img-upload__cancel');
 const effectPreview = document.querySelectorAll('.effects__preview');
 
 const uploadModal = createModalController({
@@ -21,12 +21,8 @@ const openUploadModal = () => {
 
 fileInput.addEventListener('change', () => {
   const file = fileInput.files[0];
-  if (!file) {
-    return;
-  }
-
+  if (!file) return;
   const imageUrl = URL.createObjectURL(file);
-
   imgPreview.src = imageUrl;
   effectPreview.forEach((preview) => {
     preview.style.backgroundImage = `url(${imageUrl})`;
